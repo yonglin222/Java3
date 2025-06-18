591a124bf7644dad2ef385f1e8b59d53594e9d64
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-18 15:59:35 +0900

    Update Ch4_Example.java

--- src/a3_control/Ch4_Example.java
+++ src/a3_control/Ch4_Example.java
@@ -13,5 +13,28 @@ public class Ch4_Example {
         }else {
             System.out.println("F학점");
         }
+
+        // Q2
+        // switch case문은 범위조건을 구현하기에 적합하지 않음
+        // 그래서 점수를 10을 나눠서 앞자리로 구분하는 방법을 사용함
+        switch (score/10) {
+            case 10:
+            case 9:
+                System.out.println("A학점"); break;
+            case 8:
+                System.out.println("B학점"); break;
+            case 7:
+                System.out.println("C학점"); break;
+            default:
+                System.out.println("F학점");
+        }
     }
 }
+
+
+
+
+
+
+
+
