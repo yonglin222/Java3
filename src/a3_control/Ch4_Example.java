4f202723f3b0934a35ff7e01bc0bf1c3b09f0b82
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-19 17:06:05 +0900

    Update Ch4_Example.java

--- src/a3_control/Ch4_Example.java
+++ src/a3_control/Ch4_Example.java
@@ -44,10 +44,43 @@ public class Ch4_Example {
 //        for (int i=10; i>0; i-=2) {
 //            System.out.println(i);
 //        }
-        int i = 10;
-        while (i > 0) {
-            System.out.println(i);
-            i -= 2;
+//        int i = 10;
+//        while (i > 0) {
+//            System.out.println(i);
+//            i -= 2;
+//        }
+
+        // Q5
+        for (int i=0; i<5; i++) {
+            for (int j=0; j<3; j++) {
+                if (i == 2) {
+                    continue;
+                }
+                if (j == 1) {
+                    break;
+                }
+                System.out.println("A");
+            }
+        }
+
+        // Q6
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<5; j++) {
+                if (i == 3 && j == 2) {
+                    i = 999;
+                    break;
+                }
+            }
+        }
+
+        // Q7 레이블
+        out:
+        for (int i=0; i<10; i++) {
+            for (int j=0; j<5; j++) {
+                if (i == 3 && j == 2) {
+                    break out;
+                }
+            }
         }
     }
 }
