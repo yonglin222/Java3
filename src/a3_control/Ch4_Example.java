0d24b451a60cf899e8ab90b52624ec0d339d6143
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-19 16:47:20 +0900

    Update Ch4_Example.java

--- src/a3_control/Ch4_Example.java
+++ src/a3_control/Ch4_Example.java
@@ -41,7 +41,14 @@ public class Ch4_Example {
         }
 
         // Q4
-        
+//        for (int i=10; i>0; i-=2) {
+//            System.out.println(i);
+//        }
+        int i = 10;
+        while (i > 0) {
+            System.out.println(i);
+            i -= 2;
+        }
     }
 }
 
