2ba354650bda609c672cc3d5c4c5c6bb80db85ed
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-20 16:19:57 +0900

    update

---
+++ src/a4_method/Example2.java
@@ -0,0 +1,19 @@
+package a4_method;
+
+public class Example2 {
+    public static void main(String[] args) {
+        // 주어진 숫자가 양수인지 음수인지 확인하는 메서드
+        String str = checkPositiveOrNegative(10);
+        System.out.println(str);
+        str = checkPositiveOrNegative(-5);
+        System.out.println(str);
+        str = checkPositiveOrNegative(0);
+        System.out.println(str);
+    }
+
+    public static String checkPositiveOrNegative(int num) {
+        // 매개변수 num이 양수이면 "양수", 음수이면 "음수", 0이면 "0"을
+        // 반환하도록 코드를 작성하세요.
+        return "";
+    }
+}
