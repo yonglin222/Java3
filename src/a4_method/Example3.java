2cb9fbf69c3e64376e54754559c7dc521ebc793d
hyungwonseo <144635535+hyungwonseo@users.noreply.github.com>
2025-06-20 16:24:12 +0900

    Update Example3.java

--- src/a4_method/Example3.java
+++ src/a4_method/Example3.java
@@ -2,6 +2,13 @@ package a4_method;
 
 public class Example3 {
     public static void main(String[] args) {
-
+        // 숫자를 제곱하는 메서드
+        System.out.println(square(4)); // 16
+        System.out.println(square(9)); // 81
+        System.out.println(square(15)); // 15
+    }
+    public static int square(int num) {
+        // 코드를 작성하여 매개변수의 제곱을 반환
+        return 0;
     }
 }
